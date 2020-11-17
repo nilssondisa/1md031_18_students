85970cd25c3aacdd0c25c133d3432a3c7d70f360
nilssondisa <Disa.Nilsson.2415@student.uu.se>
2020-11-17 15:04:27 +0100

    Add menu.js

---
+++ public/js/menu.js
@@ -0,0 +1,44 @@
+var food = [
+  {
+  name: "Silvias Smaskiga",
+  kCal: 750,
+  lactose: true,
+  gluten: false,
+  img: "https://whoabella.com/wp-content/uploads/2014/01/thenutellaburger.png",
+  ing :"Chokladkött och glass"
+},
+{
+name: "Royal-Snack-Attack",
+kCal: 55,
+lactose: false,
+gluten: false,
+img: "https://www.partyhallen.se/cache/d7/799x799-Produktbilder_Asba_godis-hamburgare-mini-2.jpg",
+ing:"Lokalproducerad gummi"
+},
+{
+name: "Queen-ElizaBurger",
+kCal: 10000,
+lactose: true,
+gluten: true,
+img: "https://cdn3.cdnme.se/cdn/6-2/112338/images/2008/hamburgare_allt_19890169.jpg",
+ing :"Lyxig ost och fint kött"
+},
+{
+name: "Lady-D",
+kCal: 3,
+lactose: false,
+gluten: true,
+img: "https://whoabella.com/wp-content/uploads/2014/01/thenutellaburger.png",
+ing :"Mumsigheter"
+},
+{
+name: "Pöbel-Bugaren",
+kCal: 400,
+lactose: true,
+gluten: true,
+img: "https://whoabella.com/wp-content/uploads/2014/01/thenutellaburger.png",
+ing :"Svampar"
+}
+
+
+]
